778c2b6e4a43d9bbb8243adae628690f6c678a11
nqp <nqp_java@163.com>
2021-03-12 13:10:36 +0800

    封装page

---
+++ src/main/java/com/demo/test/pages/SearchPage.java
@@ -0,0 +1,17 @@
+package com.demo.test.pages;
+
+import org.openqa.selenium.By;
+
+
+public class SearchPage {
+
+    /**
+     * 搜索输入框
+     */
+    public static final By SEARCH_INPUT=By.id("kw");
+
+    /**
+     * 搜索按钮
+     */
+    public static final By SERRCH_BUTTON=By.id("su");
+}
